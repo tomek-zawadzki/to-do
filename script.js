f53fe0aa81698f837f64c33518459fe41e47cc95
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-10-07 22:18:35 +0200

    implement adding new task

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const text = document.querySelector(".app__task");
+const btnAdd = document.querySelector(".app__btn--add");
+const btnApprove = document.querySelector(".app__btn--approve");
+const btnRemove = document.querySelector(".app__btn--remove");
+const btnClear = document.querySelector(".app__btn--clear");
+
+btnAdd.addEventListener("click", () => {
+  if (!text.value == "") {
+    const taskToDo = document.querySelector(".app__label--js");
+    taskToDo.insertAdjacentHTML(
+      "afterend",
+      `
+<li class="app__list-todo">
+${text.value}
+<div>
+  <button class="app__btn--approve">✅</button>
+  <button class="app__btn--remove">🗑</button>
+  </div>
+  `
+    );
+
+    text.value = "";
+  }
+});
