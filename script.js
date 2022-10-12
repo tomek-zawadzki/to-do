61012101d272887eccdae124a8f41bbb6977669c
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-10-12 19:51:31 +0200

    small improvment with buttons

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 "use strict";
 
+// const arr = [1, 2, 2];
+// const fuck = arr.map((n) => n ** 2);
+// console.log(fuck);
+
 const text = document.querySelector(".app__task");
 const btnAdd = document.querySelector(".app__btn--add");
-const btnApprove = document.querySelector(".app__btn--approve");
-const btnRemove = document.querySelector(".app__btn--remove");
 const btnClear = document.querySelector(".app__btn--clear");
 
 btnAdd.addEventListener("click", () => {
-  if (!text.value == "") {
-    const taskToDo = document.querySelector(".app__label--js");
-    taskToDo.insertAdjacentHTML(
-      "afterend",
-      `
+  const taskToDo = document.querySelector(".app__label--js");
+  const html = `
   <li class="app__list-todo">
   ${text.value}
   <div>
     <button class="app__btn--approve">✅</button>
     <button class="app__btn--remove">🗑</button>
     </div>
-    `
-    );
+    </li>
+    `;
+
+  if (!text.value == "") {
+    taskToDo.insertAdjacentHTML("afterend", html);
 
     text.value = "";
   } else {
     alert("Field 'Create task' cannot be empty");
   }
+
+  const btnRemove = document.querySelector(".app__btn--remove");
+  btnRemove.addEventListener("click", () => { 
+    console.log(html)
+  });
+
+  const btnApprove = document.querySelector(".app__btn--approve");
+  btnApprove.addEventListener("click", () => {
+    console.log("approve");
+  });
 });
 
 // const newLi = document.createElement("li");
